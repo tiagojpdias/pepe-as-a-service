65380a032cc056d0a7b8b680dfedb3df273a2d6b
Tiago Dias <tiagojpdias@gmail.com>
2019-02-26 11:15:12 +0000

    feat(Linters): prettier and eslint

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
+
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-client.login('NTQ5ODkxNzUzODM0OTcxMTQ3.D1afrw.q85S5b3pDvziJD2kwbEJydDd1iY');
+client.login(process.env.BOT_TOKEN);
 
-client.on('ready', () => { console.log('The bot is ready'); });
+client.on('ready', () => {
+  console.log('The bot is ready');
+});
 
 client.on('message', (msg) => {
-	if (msg.content.includes('pepe')) {
-		    msg.reply('oi');
-	}
+  if (msg.content.includes('pepe')) {
+    msg.reply('oi');
+  }
 });
