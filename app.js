9579f3c3989185a9a3adc7b326dd4cb5878a02a8
Carlos Martins <carlos@ynnovation.pt>
2019-02-26 11:45:20 +0000

    feat(Command): Add logic to bot, it'll only reply if he's tagged, send dummy image

--- app.js
+++ app.js
@@ -7,11 +7,21 @@ const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
-  console.log('The bot is ready');
+  console.log(`PEPE READY`);
+  client.user.setActivity('Pepe as a Service');
 });
 
-client.on('message', (msg) => {
-  if (msg.content.includes('pepe')) {
-    msg.reply('oi');
+client.on('message', async message => {
+  if (message.author.bot) return;
+
+  if (message.content.includes(client.user.toString())) {
+    const splitMessage = message.content.split(' ');
+    const tag = splitMessage[1];
+
+    message.channel.send('PEPE INCOMMING', {
+      files: [
+        'https://i.kym-cdn.com/photos/images/original/001/213/604/4c6.png'
+      ]
+    });
   }
 });
