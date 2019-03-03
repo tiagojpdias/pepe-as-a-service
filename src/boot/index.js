a2c18dc259e56646b773440772bf1aa78c69e464
Tiago Dias <tiagojpdias@gmail.com>
2019-03-03 18:22:38 +0000

    feat(Tags): reload tags every 10 seconds

--- src/boot/index.js
+++ src/boot/index.js
@@ -1,14 +1,25 @@
 const fs = require('fs');
 
-const file = fs.readFileSync('tags.json', 'utf8');
-const parsedFile = JSON.parse(file);
-const tags = new Map(Object.entries(parsedFile));
+let tags = new Map();
 
-const availableTags = [...tags.keys()];
-const allowedTags = availableTags.filter(tag => tag !== '__reserved');
+function reloadTags() {
+  const file = fs.readFileSync('tags.json', 'utf8');
+  const parsedFile = JSON.parse(file);
+
+  tags = Object.entries(parsedFile).reduce((newMap, [key, value]) => {
+    newMap.set(key, value);
+    return newMap;
+  }, new Map());
+}
+
+reloadTags();
+
+setInterval(reloadTags, 10 * 1000);
+
+function getTags() {
+  return tags;
+}
 
 module.exports = {
-  allowedTags,
-  availableTags,
-  tags,
+  getTags,
 };
--- src/events/message.js
+++ src/events/message.js
@@ -1,7 +1,7 @@
 const { Attachment } = require('discord.js');
 const config = require('../../config');
 const { getImage, getReservedImage } = require('../utils');
-const { allowedTags } = require('../boot');
+const { getTags } = require('../boot');
 
 const retries = new Map();
 
@@ -69,6 +69,10 @@ module.exports = async message => {
 
       return;
     }
+
+    const allowedTags = [...getTags().keys()].filter(
+      key => key !== '__reserved',
+    );
 
     if (!allowedTags.includes(tag)) {
       message.channel.send(`Tags :: ${allowedTags.join(' , ')}`);
