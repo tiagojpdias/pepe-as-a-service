339e7a85b1cd7d4619158a7a8ed6d09bacb626f4
Tiago Dias <tiagojpdias@gmail.com>
2019-03-06 17:12:58 +0000

    feat(Typescript): convert all codebase to TS

---
+++ src/events/index.ts
@@ -0,0 +1,7 @@
+import messageEvent from './message';
+import readyEvent from './ready';
+
+export {
+  readyEvent as ready,
+  messageEvent as message,
+};
---
+++ src/utils/logger.ts
@@ -0,0 +1,19 @@
+import winston from 'winston';
+import config from '../../config';
+
+const logsPath: string = config('logs.path');
+
+const logger = winston.createLogger({
+  format: winston.format.json(),
+  level: 'info',
+  transports: [
+    new winston.transports.File({filename: `${logsPath}/error.log`, level: 'error' }),
+    new winston.transports.File({filename: `${logsPath}/combined.log`}),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({ format: winston.format.simple() }));
+}
+
+export default logger;
