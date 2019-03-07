6d28c66f0ac517cb68d249cc86db08ec49f94e35
Tiago Dias <tiagojpdias@gmail.com>
2019-03-07 10:48:59 +0000

    chore(TS): add enums to avoid hardcoded strings

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,20 +1,36 @@
-import { Client, ClientUser, Presence } from 'discord.js';
+import {
+  Attachment,
+  Client,
+  ClientUser,
+  Presence,
+  TextChannel,
+} from 'discord.js';
 import config from '../../config';
-import { logger } from '../utils';
+import { getReservedImage, logger } from '../utils';
+import { MessageType, PresenceStatusType } from '../utils/enums';
 
 const currentDate: string = new Date(Date.now()).toLocaleString('PT');
+const messagesEnabled: boolean = config('messages.enabled') as boolean;
 
 async function readyEvent(client: Client) {
   const { user }: Client = client;
 
   await user.setActivity(config('activities.enter') as string);
-  await user.setStatus('online');
+  await user.setStatus(PresenceStatusType.online);
+
+  if (messagesEnabled) {
+    displayMessage(client, MessageType.enter);
+  }
 
   process.on('SIGINT', async () => {
-    const newStatus: ClientUser = await user.setStatus('dnd');
+    const newStatus: ClientUser = await user.setStatus(PresenceStatusType.dnd);
     const newActivity: Presence = await user.setActivity(config(
       'activities.exit',
     ) as string);
+
+    if (messagesEnabled) {
+      displayMessage(client, MessageType.exit);
+    }
 
     if (newStatus && newActivity) {
       process.exit(0);
@@ -22,6 +38,34 @@ async function readyEvent(client: Client) {
   });
 
   logger.info(`Pepe-As-A-Service started @ ${currentDate}`);
+}
+
+function displayMessage(client: Client, type: MessageType): void {
+  const mainChannelId: string = config('bot.mainChannelId') as string;
+  const channel: TextChannel = client.channels.get(
+    mainChannelId,
+  ) as TextChannel;
+
+  switch (type) {
+    case MessageType.enter: {
+      const helloImage = new Attachment(getReservedImage('hello'));
+
+      channel.send(config('messages.enter'), {
+        files: [helloImage],
+      });
+      break;
+    }
+    case MessageType.exit: {
+      const goodbyeImage = new Attachment(getReservedImage('goodbye'));
+
+      channel.send(config('messages.leave'), {
+        files: [goodbyeImage],
+      });
+    }
+    default: {
+      break;
+    }
+  }
 }
 
 export default readyEvent;
