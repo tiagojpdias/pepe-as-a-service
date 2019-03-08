be6eed4f5b0459c61b5f84cb41c1ef1505031dc5
Tiago Dias <tiagojpdias@gmail.com>
2019-03-08 18:44:53 +0000

    test(Config): test to reach full coverage

--- __tests__/config.test.ts
+++ __tests__/config.test.ts
@@ -1,11 +1,20 @@
 import config from '../config';
 
 describe('Config', () => {
+  it('No file path provided', () => {
+    expect(config()).toBeUndefined();
+  });
+
   it('Access invalid file', () => {
     expect(config('invalidFile.batatas')).toBeUndefined();
   });
 
   it('Access valid file', () => {
     expect(config('images.width')).toBeDefined();
+  });
+
+  it('Access valid already loaded', () => {
+    expect(config('images.width')).toBeDefined();
+    expect(config('images.height')).toBeDefined();
   });
 });
